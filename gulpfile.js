d109a54dac332bd8c8fdc97a4a9acba876ce3922
karolklp <kklepa@gmail.com>
2015-06-18 13:49:06 +0200

    push

--- gulpfile.js
+++ gulpfile.js
@@ -236,7 +236,7 @@ gulp.task('default', ['clean'], function (cb) {
     ['copy', 'styles'],
     'elements',
     ['jshint', 'images', 'fonts', 'html'],
-    'vulcanize', 'precache',
+    'precache', 'vulcanize',
     cb);
 });
 
